65a6f0434d1cde39618ae50e23749ffd7bb1bd68
RobertaWaa <anarobiqq@gmail.com>
2025-05-10 04:55:33 +0300

    Vercel fix 1

--- vite.config.ts
+++ vite.config.ts
@@ -3,20 +3,27 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/",
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    outDir: "dist",          
+    sourcemap: mode === 'development', 
+    chunkSizeWarningLimit: 1600, 
+    rollupOptions: {        
+      external: ['@rollup/rollup-linux-x64-gnu'], 
     },
   },
 }));
