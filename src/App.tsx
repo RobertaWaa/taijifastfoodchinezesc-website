83ef6976e8ec98b2ac9b2c96c0db49ca0fda3540
RobertaWaa <anarobiqq@gmail.com>
2025-05-16 19:50:56 +0300

    Fix Vercel routing issue

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import HomePage from "./pages/HomePage";
 import MenuPage from "./pages/MenuPage";
@@ -64,7 +64,7 @@ const App = () => (
             `}
           </script>
         </Helmet>
-        <HashRouter>
+        <BrowserRouter>
   <Routes>
     <Route element={<Layout />}>
       <Route path="/" element={<HomePage />} />
@@ -76,7 +76,7 @@ const App = () => (
       <Route path="*" element={<NotFound />} />
     </Route>
   </Routes>
-</HashRouter>
+</BrowserRouter>
       </HelmetProvider>
     </TooltipProvider>
   </QueryClientProvider>
